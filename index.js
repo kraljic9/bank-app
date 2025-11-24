6213944a59353a5599d34d7401f1aa53f9744c9e
Pavle Kraljic <kraljicpavle9@gmail.com>
2025-11-24 21:53:23 +0100

    Added functionality to buttons within register and login container they will display the value of the input fields

--- index.js
+++ index.js
@@ -11,7 +11,19 @@ let registerContainer = document.querySelector(".register-container");
 // Container buttons
 let loginCancleBtn = document.querySelector(".login-cancle-btn");
 let registerCancleBtn = document.querySelector(".register-cancle-btn");
+let registerContainerBtn = document.querySelector(".register-container-btn");
+let loginContainerBtn = document.querySelector(".login-container-btn");
 
+// Register input fields
+let registerInputName = document.querySelector("#name-register");
+let passwordRegister = document.querySelector("#password-register");
+let passwordConfirm = document.querySelector("#password-confirm");
+
+// Login input fields
+let loginName = document.querySelector("#name-login");
+let passwordLogin = document.querySelector("#password-login");
+
+// Nav buttons functions
 navLoginBtn.addEventListener("click", function () {
   loginContainer.classList.toggle("display-none");
 });
@@ -20,6 +32,7 @@ navRegisterBtn.addEventListener("click", function () {
   registerContainer.classList.toggle("display-none");
 });
 
+// Main buttons functions
 mainLoginBtn.addEventListener("click", function () {
   loginContainer.classList.toggle("display-none");
 });
@@ -34,4 +47,19 @@ loginCancleBtn.addEventListener("click", function () {
 
 registerCancleBtn.addEventListener("click", function () {
   registerContainer.classList.toggle("display-none");
+});
+
+// Register Container Btn function
+
+registerContainerBtn.addEventListener("click", function () {
+  console.log(registerInputName.value);
+  console.log(passwordRegister.value);
+  console.log(passwordConfirm.value);
+});
+
+// Login Container Btn function
+
+loginContainerBtn.addEventListener("click", function () {
+  console.log(loginName.value);
+  console.log(passwordLogin.value);
 });
