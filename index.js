013dec21b6d264b37f6b6e0dccea80f7e7dec7cb
Pavle Kraljic <kraljicpavle9@gmail.com>
2025-11-24 21:27:18 +0100

    Updated class names for cancle container buttons, added functionality to buttons

--- index.js
+++ index.js
@@ -8,6 +8,30 @@ let mainRegisterBtn = document.querySelector(".main-register");
 let loginContainer = document.querySelector(".login-container");
 let registerContainer = document.querySelector(".register-container");
 
+// Container buttons
+let loginCancleBtn = document.querySelector(".login-cancle-btn");
+let registerCancleBtn = document.querySelector(".register-cancle-btn");
+
 navLoginBtn.addEventListener("click", function () {
   loginContainer.classList.toggle("display-none");
+});
+
+navRegisterBtn.addEventListener("click", function () {
+  registerContainer.classList.toggle("display-none");
+});
+
+mainLoginBtn.addEventListener("click", function () {
+  loginContainer.classList.toggle("display-none");
+});
+
+mainRegisterBtn.addEventListener("click", function () {
+  registerContainer.classList.toggle("display-none");
+});
+
+loginCancleBtn.addEventListener("click", function () {
+  loginContainer.classList.toggle("display-none");
+});
+
+registerCancleBtn.addEventListener("click", function () {
+  registerContainer.classList.toggle("display-none");
 });
